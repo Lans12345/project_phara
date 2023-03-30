3dee60a51bbc4218d3322c0f80a28f7a96d02f1c
Lans12345 <olanalans12345@gmail.com>
2023-03-30 16:39:48 +0800

    added background

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -13,63 +13,73 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: grey,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 50,
-              ),
-              TextBold(text: 'PHara', fontSize: 58, color: Colors.white),
-              const SizedBox(
-                height: 75,
-              ),
-              TextRegular(text: 'Login', fontSize: 24, color: Colors.white),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  hint: 'Email', label: 'Email', controller: emailController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  showEye: true,
-                  isObscure: true,
-                  hint: 'Password',
-                  label: 'Password',
-                  controller: passwordController),
-              const SizedBox(
-                height: 25,
-              ),
-              Center(
-                child: ButtonWidget(
-                  color: black,
-                  label: 'Login',
-                  onPressed: (() {}),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/back.png'),
+                fit: BoxFit.cover)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 50,
                 ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextRegular(
-                      text: "New to PHara?", fontSize: 12, color: Colors.white),
-                  TextButton(
-                    onPressed: (() {
-                      Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => SignupScreen()));
-                    }),
-                    child: TextBold(
-                        text: "Signup Now", fontSize: 14, color: Colors.white),
+                TextBold(text: 'PHara', fontSize: 58, color: Colors.white),
+                const SizedBox(
+                  height: 75,
+                ),
+                TextRegular(text: 'Login', fontSize: 24, color: Colors.white),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    hint: 'Email', label: 'Email', controller: emailController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    showEye: true,
+                    isObscure: true,
+                    hint: 'Password',
+                    label: 'Password',
+                    controller: passwordController),
+                const SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: ButtonWidget(
+                    color: black,
+                    label: 'Login',
+                    onPressed: (() {}),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextRegular(
+                        text: "New to PHara?",
+                        fontSize: 12,
+                        color: Colors.white),
+                    TextButton(
+                      onPressed: (() {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => SignupScreen()));
+                      }),
+                      child: TextBold(
+                          text: "Signup Now",
+                          fontSize: 14,
+                          color: Colors.white),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -18,97 +18,103 @@ class SignupScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: grey,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              TextBold(text: 'PHara', fontSize: 58, color: Colors.white),
-              const SizedBox(
-                height: 25,
-              ),
-              TextRegular(text: 'Signup', fontSize: 24, color: Colors.white),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(label: 'Name', controller: nameController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  inputType: TextInputType.number,
-                  label: 'Mobile Number',
-                  controller: numberController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  inputType: TextInputType.streetAddress,
-                  label: 'Address',
-                  controller: addressController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  inputType: TextInputType.streetAddress,
-                  label: 'Email',
-                  controller: emailController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  showEye: true,
-                  isObscure: true,
-                  inputType: TextInputType.streetAddress,
-                  label: 'Password',
-                  controller: passwordController),
-              const SizedBox(
-                height: 10,
-              ),
-              TextFieldWidget(
-                  showEye: true,
-                  isObscure: true,
-                  inputType: TextInputType.streetAddress,
-                  label: 'Confirm Password',
-                  controller: confirmPasswordController),
-              const SizedBox(
-                height: 25,
-              ),
-              Center(
-                child: ButtonWidget(
-                  color: black,
-                  label: 'Signup',
-                  onPressed: (() {
-                    Navigator.of(context).pushReplacement(MaterialPageRoute(
-                        builder: (context) => const HomeScreen()));
-                  }),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/back.png'),
+                fit: BoxFit.cover)),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 50, 30, 50),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextRegular(
-                      text: "Already have an Account?",
-                      fontSize: 12,
-                      color: Colors.white),
-                  TextButton(
+                TextBold(text: 'PHara', fontSize: 58, color: Colors.white),
+                const SizedBox(
+                  height: 25,
+                ),
+                TextRegular(text: 'Signup', fontSize: 24, color: Colors.white),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(label: 'Name', controller: nameController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    inputType: TextInputType.number,
+                    label: 'Mobile Number',
+                    controller: numberController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    inputType: TextInputType.streetAddress,
+                    label: 'Address',
+                    controller: addressController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    inputType: TextInputType.streetAddress,
+                    label: 'Email',
+                    controller: emailController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    showEye: true,
+                    isObscure: true,
+                    inputType: TextInputType.streetAddress,
+                    label: 'Password',
+                    controller: passwordController),
+                const SizedBox(
+                  height: 10,
+                ),
+                TextFieldWidget(
+                    showEye: true,
+                    isObscure: true,
+                    inputType: TextInputType.streetAddress,
+                    label: 'Confirm Password',
+                    controller: confirmPasswordController),
+                const SizedBox(
+                  height: 25,
+                ),
+                Center(
+                  child: ButtonWidget(
+                    color: black,
+                    label: 'Signup',
                     onPressed: (() {
                       Navigator.of(context).pushReplacement(MaterialPageRoute(
-                          builder: (context) => LoginScreen()));
+                          builder: (context) => const HomeScreen()));
                     }),
-                    child: TextBold(
-                        text: "Login Now", fontSize: 14, color: Colors.white),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextRegular(
+                        text: "Already have an Account?",
+                        fontSize: 12,
+                        color: Colors.white),
+                    TextButton(
+                      onPressed: (() {
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                            builder: (context) => LoginScreen()));
+                      }),
+                      child: TextBold(
+                          text: "Login Now", fontSize: 14, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
