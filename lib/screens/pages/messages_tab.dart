fb0e28baf1be70bd322a707da31d4ffab3d1adf8
Lans12345 <olanalans12345@gmail.com>
2023-03-31 19:27:43 +0800

    added messages tab

--- lib/screens/pages/messages_tab.dart
+++ lib/screens/pages/messages_tab.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:phara/utils/colors.dart';
+import 'package:phara/widgets/text_widget.dart';
 
 import '../../widgets/appbar_widget.dart';
 import '../../widgets/drawer_widget.dart';
@@ -11,6 +13,45 @@ class MessagesTab extends StatelessWidget {
     return Scaffold(
       drawer: const DrawerWidget(),
       appBar: AppbarWidget('Messages'),
+      body: ListView.separated(
+          itemCount: 100,
+          separatorBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20),
+              child: Divider(
+                color: grey,
+              ),
+            );
+          },
+          itemBuilder: ((context, index) {
+            return Padding(
+              padding: const EdgeInsets.fromLTRB(20, 5, 20, 5),
+              child: ListTile(
+                leading: const CircleAvatar(
+                  maxRadius: 25,
+                  minRadius: 25,
+                  backgroundImage: AssetImage(
+                    'assets/images/profile.png',
+                  ),
+                ),
+                title: TextBold(text: 'Lance Olana', fontSize: 15, color: grey),
+                subtitle: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextRegular(
+                        text: 'Sample message right here',
+                        fontSize: 12,
+                        color: grey),
+                    TextRegular(text: '2:30 PM', fontSize: 12, color: grey),
+                  ],
+                ),
+                trailing: const Icon(
+                  Icons.arrow_right,
+                  color: grey,
+                ),
+              ),
+            );
+          })),
     );
   }
 }
