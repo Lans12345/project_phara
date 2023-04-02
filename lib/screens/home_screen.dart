587b81468c90608be9f7be60373819551abf37f5
Lans12345 <olanalans12345@gmail.com>
2023-04-02 15:53:17 +0800

    added my location func

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:phara/plugins/my_location.dart';
 import 'package:phara/screens/pages/bookmark_page.dart';
 import 'package:phara/utils/colors.dart';
 import 'package:phara/widgets/book_bottomsheet_widget.dart';
@@ -14,14 +17,26 @@ import 'package:uuid/uuid.dart';
 import '../widgets/delegate/search_my_places.dart';
 import 'pages/messages_tab.dart';
 
-class HomeScreen extends StatelessWidget {
-  final Completer<GoogleMapController> _controller =
-      Completer<GoogleMapController>();
-
+class HomeScreen extends StatefulWidget {
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
     zoom: 14.4746,
   );
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    determinePosition();
+    getLocation();
+  }
+
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
 
   final List<LatLng> _markerLocations = [
     const LatLng(37.4220, -122.0841),
@@ -35,6 +50,174 @@ class HomeScreen extends StatelessWidget {
     const LatLng(37.8044, -122.2711),
     const LatLng(37.8716, -122.2727),
   ];
+
+  late String currentAddress;
+
+  late double lat = 0;
+  late double long = 0;
+
+  var hasLoaded = false;
+
+  GoogleMapController? mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    final CameraPosition camPosition = CameraPosition(
+      target: LatLng(lat, long),
+      zoom: 14.4746,
+    );
+    return hasLoaded
+        ? Scaffold(
+            floatingActionButton: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: (() {}),
+                    child: const Icon(
+                      Icons.pin_drop_rounded,
+                      color: Colors.red,
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: (() {}),
+                    child: const Icon(
+                      Icons.push_pin_rounded,
+                      color: grey,
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: (() {
+                      mapController?.animateCamera(
+                          CameraUpdate.newCameraPosition(CameraPosition(
+                              target: LatLng(lat, long), zoom: 16)));
+                    }),
+                    child: const Icon(
+                      Icons.my_location_rounded,
+                      color: grey,
+                    )),
+                const SizedBox(
+                  height: 15,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.white,
+                    onPressed: (() {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const BookmarksPage()));
+                    }),
+                    child: const Icon(
+                      Icons.collections_bookmark_outlined,
+                      color: grey,
+                    )),
+              ],
+            ),
+            drawer: const Drawer(
+              child: DrawerWidget(),
+            ),
+            appBar: AppBar(
+              foregroundColor: grey,
+              backgroundColor: Colors.white,
+              title: GestureDetector(
+                onTap: () async {
+                  final sessionToken = const Uuid().v4();
+
+                  await showSearch(
+                      context: context,
+                      delegate: LocationsSearch(sessionToken));
+                },
+                child: TextFormField(
+                  enabled: false,
+                  decoration: const InputDecoration.collapsed(
+                    hintText: "Search Location",
+                    hintStyle: TextStyle(fontFamily: 'QBold', color: grey),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              actions: [
+                IconButton(
+                  onPressed: (() {}),
+                  icon: const Icon(Icons.pin_drop_outlined),
+                ),
+                Badge(
+                  position: BadgePosition.custom(start: -1, top: 3),
+                  badgeContent: TextRegular(
+                    text: '1',
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                  child: IconButton(
+                    onPressed: (() {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => MessagesTab()));
+                    }),
+                    icon: const Icon(Icons.message_outlined),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+              ],
+            ),
+            body: Stack(
+              children: [
+                GoogleMap(
+                  myLocationButtonEnabled: false,
+                  myLocationEnabled: true,
+                  markers: _createMarkers(context),
+                  zoomControlsEnabled: false,
+                  mapType: MapType.normal,
+                  initialCameraPosition: camPosition,
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                    setState(() {
+                      mapController = controller;
+                    });
+                  },
+                ),
+                Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ButtonWidget(
+                          width: 175,
+                          radius: 100,
+                          opacity: 1,
+                          color: Colors.red,
+                          label: 'Clear pin',
+                          onPressed: (() {})),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      ButtonWidget(
+                          width: 175,
+                          radius: 100,
+                          opacity: 1,
+                          color: Colors.green,
+                          label: 'Book a ride',
+                          onPressed: (() {})),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        : const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+  }
 
   Set<Marker> _createMarkers(context) {
     return _markerLocations.map((location) {
@@ -62,143 +245,21 @@ class HomeScreen extends StatelessWidget {
     }).toSet();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: (() {}),
-              child: const Icon(
-                Icons.pin_drop_rounded,
-                color: Colors.red,
-              )),
-          const SizedBox(
-            height: 15,
-          ),
-          FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: (() {}),
-              child: const Icon(
-                Icons.push_pin_rounded,
-                color: grey,
-              )),
-          const SizedBox(
-            height: 15,
-          ),
-          FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: (() {}),
-              child: const Icon(
-                Icons.my_location_rounded,
-                color: grey,
-              )),
-          const SizedBox(
-            height: 15,
-          ),
-          FloatingActionButton(
-              backgroundColor: Colors.white,
-              onPressed: (() {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                    builder: (context) => const BookmarksPage()));
-              }),
-              child: const Icon(
-                Icons.collections_bookmark_outlined,
-                color: grey,
-              )),
-        ],
-      ),
-      drawer: const Drawer(
-        child: DrawerWidget(),
-      ),
-      appBar: AppBar(
-        foregroundColor: grey,
-        backgroundColor: Colors.white,
-        title: GestureDetector(
-          onTap: () async {
-            final sessionToken = const Uuid().v4();
+  getLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
 
-            await showSearch(
-                context: context, delegate: LocationsSearch(sessionToken));
-          },
-          child: TextFormField(
-            enabled: false,
-            decoration: const InputDecoration.collapsed(
-              hintText: "Search Location",
-              hintStyle: TextStyle(fontFamily: 'QBold', color: grey),
-              border: InputBorder.none,
-            ),
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: (() {}),
-            icon: const Icon(Icons.pin_drop_outlined),
-          ),
-          Badge(
-            position: BadgePosition.custom(start: -1, top: 3),
-            badgeContent: TextRegular(
-              text: '1',
-              fontSize: 12,
-              color: Colors.white,
-            ),
-            child: IconButton(
-              onPressed: (() {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => MessagesTab()));
-              }),
-              icon: const Icon(Icons.message_outlined),
-            ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          GoogleMap(
-            myLocationEnabled: true,
-            markers: _createMarkers(context),
-            zoomControlsEnabled: false,
-            mapType: MapType.normal,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) {
-              _controller.complete(controller);
-            },
-          ),
-          Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ButtonWidget(
-                    width: 175,
-                    radius: 100,
-                    opacity: 1,
-                    color: Colors.red,
-                    label: 'Clear pin',
-                    onPressed: (() {})),
-                const SizedBox(
-                  height: 20,
-                ),
-                ButtonWidget(
-                    width: 175,
-                    radius: 100,
-                    opacity: 1,
-                    color: Colors.green,
-                    label: 'Book a ride',
-                    onPressed: (() {})),
-                const SizedBox(
-                  height: 25,
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    );
+    List<Placemark> p =
+        await placemarkFromCoordinates(position.latitude, position.longitude);
+
+    Placemark place = p[0];
+
+    setState(() {
+      lat = position.latitude;
+      long = position.longitude;
+      currentAddress =
+          '${place.street}, ${place.subLocality}, ${place.locality}';
+      hasLoaded = true;
+    });
   }
 }
