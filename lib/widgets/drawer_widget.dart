4e6f7ff055246b23ac7dffa915b8e390afb4e256
Lans12345 <olanalans12345@gmail.com>
2023-04-02 10:06:05 +0800

    displayed user data

--- lib/widgets/drawer_widget.dart
+++ lib/widgets/drawer_widget.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:phara/screens/auth/login_screen.dart';
 import 'package:phara/screens/home_screen.dart';
@@ -10,194 +12,213 @@ class DrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      child: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.only(top: 0),
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-              ),
-              accountEmail: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 10,
+    final Stream<DocumentSnapshot> userData = FirebaseFirestore.instance
+        .collection('Users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots();
+    return StreamBuilder<DocumentSnapshot>(
+        stream: userData,
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: Text('Loading'));
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('Something went wrong'));
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          dynamic data = snapshot.data;
+          return SizedBox(
+            child: Drawer(
+              child: ListView(
+                padding: const EdgeInsets.only(top: 0),
+                children: <Widget>[
+                  UserAccountsDrawerHeader(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    accountEmail: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(
+                              Icons.phone,
+                              color: Colors.grey,
+                              size: 15,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            TextRegular(
+                              text: data['number'],
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Icon(
+                              Icons.email,
+                              color: Colors.grey,
+                              size: 15,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            TextRegular(
+                              text: data['email'],
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    accountName: Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: TextBold(
+                        text: data['name'],
+                        fontSize: 18,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    currentAccountPicture: const Padding(
+                      padding: EdgeInsets.all(5.0),
+                      child: CircleAvatar(
+                        minRadius: 75,
+                        maxRadius: 75,
+                        backgroundImage:
+                            AssetImage('assets/images/profile.png'),
+                      ),
+                    ),
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Icon(
-                        Icons.phone,
-                        color: Colors.grey,
-                        size: 15,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      TextRegular(
-                        text: '09090104355',
-                        fontSize: 14,
-                        color: Colors.grey,
-                      ),
-                    ],
+                  ListTile(
+                    leading: const Icon(Icons.home),
+                    title: TextRegular(
+                      text: 'Home',
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => HomeScreen()));
+                    },
                   ),
-                  const SizedBox(
-                    height: 5,
+                  ListTile(
+                    leading: const Icon(Icons.message_outlined),
+                    title: TextRegular(
+                      text: 'Messages',
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => MessagesTab()));
+                    },
                   ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Icon(
-                        Icons.email,
-                        color: Colors.grey,
-                        size: 15,
-                      ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      TextRegular(
-                        text: 'olanalans12345@gmail.com',
-                        fontSize: 14,
-                        color: Colors.grey,
-                      ),
-                    ],
+                  ListTile(
+                    leading: const Icon(Icons.tab_rounded),
+                    title: TextRegular(
+                      text: 'Recent trips',
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    onTap: () {
+                      Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (context) => const TripsPage()));
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.manage_accounts_outlined,
+                    ),
+                    title: TextRegular(
+                      text: 'Contact us',
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacement(
+                      //     MaterialPageRoute(builder: (context) => HomeScreen()));
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
+                      Icons.info_outline_rounded,
+                    ),
+                    title: TextRegular(
+                      text: 'About us',
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    onTap: () {
+                      // Navigator.of(context).pushReplacement(
+                      //     MaterialPageRoute(builder: (context) => HomeScreen()));
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.logout),
+                    title: TextRegular(
+                      text: 'Logout',
+                      fontSize: 12,
+                      color: Colors.grey,
+                    ),
+                    onTap: () {
+                      showDialog(
+                          context: context,
+                          builder: (context) => AlertDialog(
+                                title: const Text(
+                                  'Logout Confirmation',
+                                  style: TextStyle(
+                                      fontFamily: 'QBold',
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                content: const Text(
+                                  'Are you sure you want to Logout?',
+                                  style: TextStyle(fontFamily: 'QRegular'),
+                                ),
+                                actions: <Widget>[
+                                  FlatButton(
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                    child: const Text(
+                                      'Close',
+                                      style: TextStyle(
+                                          fontFamily: 'QRegular',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  FlatButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  LoginScreen()));
+                                    },
+                                    child: const Text(
+                                      'Continue',
+                                      style: TextStyle(
+                                          fontFamily: 'QRegular',
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                ],
+                              ));
+                    },
                   ),
                 ],
               ),
-              accountName: Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: TextBold(
-                  text: 'Lance Olana',
-                  fontSize: 18,
-                  color: Colors.grey,
-                ),
-              ),
-              currentAccountPicture: const Padding(
-                padding: EdgeInsets.all(5.0),
-                child: CircleAvatar(
-                  minRadius: 75,
-                  maxRadius: 75,
-                  backgroundImage: AssetImage('assets/images/profile.png'),
-                ),
-              ),
             ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: TextRegular(
-                text: 'Home',
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => HomeScreen()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.message_outlined),
-              title: TextRegular(
-                text: 'Messages',
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => MessagesTab()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.tab_rounded),
-              title: TextRegular(
-                text: 'Recent trips',
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(builder: (context) => const TripsPage()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.manage_accounts_outlined,
-              ),
-              title: TextRegular(
-                text: 'Contact us',
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                // Navigator.of(context).pushReplacement(
-                //     MaterialPageRoute(builder: (context) => HomeScreen()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.info_outline_rounded,
-              ),
-              title: TextRegular(
-                text: 'About us',
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                // Navigator.of(context).pushReplacement(
-                //     MaterialPageRoute(builder: (context) => HomeScreen()));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: TextRegular(
-                text: 'Logout',
-                fontSize: 12,
-                color: Colors.grey,
-              ),
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                          title: const Text(
-                            'Logout Confirmation',
-                            style: TextStyle(
-                                fontFamily: 'QBold',
-                                fontWeight: FontWeight.bold),
-                          ),
-                          content: const Text(
-                            'Are you sure you want to Logout?',
-                            style: TextStyle(fontFamily: 'QRegular'),
-                          ),
-                          actions: <Widget>[
-                            FlatButton(
-                              onPressed: () => Navigator.of(context).pop(true),
-                              child: const Text(
-                                'Close',
-                                style: TextStyle(
-                                    fontFamily: 'QRegular',
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                            FlatButton(
-                              onPressed: () {
-                                Navigator.of(context).pushReplacement(
-                                    MaterialPageRoute(
-                                        builder: (context) => LoginScreen()));
-                              },
-                              child: const Text(
-                                'Continue',
-                                style: TextStyle(
-                                    fontFamily: 'QRegular',
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        ));
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
