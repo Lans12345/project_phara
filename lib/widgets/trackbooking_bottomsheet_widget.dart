fff0af2ac21cd043f7b4a834f29a52d0d7ac3fbf
Lans12345 <olanalans12345@gmail.com>
2023-04-18 18:50:15 +0800

    added ratings func

--- lib/widgets/trackbooking_bottomsheet_widget.dart
+++ lib/widgets/trackbooking_bottomsheet_widget.dart
@@ -1,14 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:phara/widgets/text_widget.dart';
 
+import '../screens/home_screen.dart';
 import '../utils/colors.dart';
 import 'button_widget.dart';
 
-class TrackBookingBottomSheetWidget extends StatelessWidget {
+class TrackBookingBottomSheetWidget extends StatefulWidget {
   final Map tripDetails;
 
   const TrackBookingBottomSheetWidget({super.key, required this.tripDetails});
 
+  @override
+  State<TrackBookingBottomSheetWidget> createState() =>
+      _TrackBookingBottomSheetWidgetState();
+}
+
+class _TrackBookingBottomSheetWidgetState
+    extends State<TrackBookingBottomSheetWidget> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -71,7 +82,7 @@ class TrackBookingBottomSheetWidget extends StatelessWidget {
                     color: Colors.red,
                   ),
                   title: TextRegular(
-                      text: 'Distance: ${tripDetails['distance']} km',
+                      text: 'Distance: ${widget.tripDetails['distance']} km',
                       fontSize: 16,
                       color: grey),
                 ),
@@ -84,7 +95,7 @@ class TrackBookingBottomSheetWidget extends StatelessWidget {
                     color: grey,
                   ),
                   title: TextRegular(
-                      text: 'From:  ${tripDetails['origin']}',
+                      text: 'From:  ${widget.tripDetails['origin']}',
                       fontSize: 16,
                       color: grey),
                 ),
@@ -97,7 +108,7 @@ class TrackBookingBottomSheetWidget extends StatelessWidget {
                     color: Colors.red,
                   ),
                   title: TextRegular(
-                      text: 'To:  ${tripDetails['destination']}',
+                      text: 'To:  ${widget.tripDetails['destination']}',
                       fontSize: 16,
                       color: grey),
                 ),
@@ -110,7 +121,7 @@ class TrackBookingBottomSheetWidget extends StatelessWidget {
                     color: grey,
                   ),
                   title: TextRegular(
-                      text: 'Fare: ₱${tripDetails['fare']}',
+                      text: 'Fare: ₱${widget.tripDetails['fare']}',
                       fontSize: 16,
                       color: grey),
                 ),
@@ -140,7 +151,77 @@ class TrackBookingBottomSheetWidget extends StatelessWidget {
                 opacity: 1,
                 color: Colors.green,
                 label: 'Confirm payment',
-                onPressed: (() {}),
+                onPressed: (() {
+                  showDialog(
+                      barrierDismissible: false,
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: TextRegular(
+                              text: 'Rate your experience',
+                              fontSize: 14,
+                              color: Colors.black),
+                          content: SizedBox(
+                            height: 50,
+                            child: Center(
+                              child: RatingBar.builder(
+                                initialRating: 5,
+                                minRating: 1,
+                                direction: Axis.horizontal,
+                                allowHalfRating: false,
+                                itemCount: 5,
+                                itemPadding:
+                                    const EdgeInsets.symmetric(horizontal: 0.0),
+                                itemBuilder: (context, _) => const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                ),
+                                onRatingUpdate: (rating) async {
+                                  int stars = 0;
+
+                                  FirebaseFirestore.instance
+                                      .collection('Drivers')
+                                      .where('id',
+                                          isEqualTo:
+                                              widget.tripDetails['driverId'])
+                                      .get()
+                                      .then((QuerySnapshot querySnapshot) {
+                                    for (var doc in querySnapshot.docs) {
+                                      setState(() {
+                                        stars = doc['stars'];
+                                      });
+                                    }
+                                  });
+                                  await FirebaseFirestore.instance
+                                      .collection('Drivers')
+                                      .doc(widget.tripDetails['driverId'])
+                                      .update({
+                                    'ratings': FieldValue.arrayUnion([
+                                      FirebaseAuth.instance.currentUser!.uid
+                                    ]),
+                                    'stars': stars + 1
+                                  });
+                                },
+                              ),
+                            ),
+                          ),
+                          actions: [
+                            TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) =>
+                                            const HomeScreen()));
+                              },
+                              child: TextBold(
+                                  text: 'Continue',
+                                  fontSize: 18,
+                                  color: Colors.amber),
+                            ),
+                          ],
+                        );
+                      });
+                }),
               ),
             ],
           ),
