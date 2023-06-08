4ffc568815aa63f2051b3ff0eac1d968cf1c61b3
Lans12345 <olanalans12345@gmail.com>
2023-06-08 16:21:19 +0800

    wip drivers profile pag

---
+++ lib/screens/pages/driver_profile_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DriverProfilePage extends StatelessWidget {
+  const DriverProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
